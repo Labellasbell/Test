4fe2932aaccd1b9304beee8321ecd07a8e6b4411
Labellasbell <31111540+Labellasbell@users.noreply.github.com>
2024-06-03 10:42:39 -0400

    Create PaymentProcessor.js

---
+++ src/check/PaymentProcessor.js
@@ -0,0 +1,9 @@
+// PaymentProcessor.js
+class PaymentProcessor {
+    processPayment(amount) {
+        // Logic to process payment
+        console.log(`Processing payment of $${amount}`);
+    }
+}
+
+module.exports = PaymentProcessor;
