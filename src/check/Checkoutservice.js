c9fdd5635621bf9a2d532133687a94aa861f30b6
Labellasbell <31111540+Labellasbell@users.noreply.github.com>
2024-06-03 10:41:23 -0400

    Create Checkoutservice.js

---
+++ src/check/Checkoutservice.js
@@ -0,0 +1,9 @@
+// CheckoutService.js
+class CheckoutService {
+    processOrder(order) {
+        // Logic to process order
+        console.log(`Processing order for ${order.customerName}`);
+    }
+}
+
+module.exports = CheckoutService;
