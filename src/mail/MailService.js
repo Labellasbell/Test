228d44f2e899901865e6381b1dba9479f7bfdc35
Labellasbell <31111540+Labellasbell@users.noreply.github.com>
2024-06-03 10:32:46 -0400

    Create MailService.js

---
+++ src/mail/MailService.js
@@ -0,0 +1,9 @@
+// MailService.js
+class MailService {
+    sendMail(to, subject, body) {
+        // Logic to send mail
+        console.log(`Sending mail to ${to} with subject ${subject}`);
+    }
+}
+
+module.exports = MailService;
